d504708b320ddc87e1b448f2d6e270171047b1c3
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2021-04-06 13:11:33 +0100

    who are we section

--- componets/Header.js
+++ componets/Header.js
@@ -2,19 +2,20 @@ import Navbar from "./navbar";
 
 function Header() {
   return (
-    <div classNames="h-auto">
+    <div classNames="h-auto relative">
       <Navbar />
       <div
         className=" bg-opacity-80  
-          absolute top-0 w-full h-full bg-center bg-cover flex  items-center "
+           w-full h-full bg-center bg-cover flex  items-center flex-col border-transparent  "
         style={{
-          backgroundImage:
-            "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')",
+          background:
+            " linear-gradient( #00438bd5, #00438bd5), url(/hero-bg-2.jpg)",
+            backgroundBlendMode:"multiply",
         }}
       >
-        <span className="w-full h-full absolute opacity-80  bg-blue-900"></span>
+        {/* <span className="w-full h-full absolute opacity-80  bg-blue-900"></span> */}
 
-        <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
+        <section className="header relative pt-16 items-center flex h-screen max-h-860-px mb-24">
           <div className="container mx-auto items-center flex flex-wrap">
             <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-12">
               <div className="pt-32 sm:pt-0">
@@ -46,21 +47,15 @@ function Header() {
           </div>
 
           <img
-            className="absolute top-2 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860px"
+            className="absolute top-6 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860px"
             src="/headerImage3.svg"
             alt="..."
           />
         </section>
+        <svg className='border-0 border-transparent overflow-hidden' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,64L40,85.3C80,107,160,149,240,149.3C320,149,400,107,480,96C560,85,640,107,720,149.3C800,192,880,256,960,240C1040,224,1120,128,1200,106.7C1280,85,1360,139,1400,165.3L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
       </div>
-      <div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path
-            fill="#00438b"
-            fill-opacity="1"
-            d="M0,256L20,256C40,256,80,256,120,229.3C160,203,200,149,240,128C280,107,320,117,360,138.7C400,160,440,192,480,181.3C520,171,560,117,600,96C640,75,680,85,720,101.3C760,117,800,139,840,165.3C880,192,920,224,960,202.7C1000,181,1040,107,1080,101.3C1120,96,1160,160,1200,165.3C1240,171,1280,117,1320,106.7C1360,96,1400,128,1420,144L1440,160L1440,0L1420,0C1400,0,1360,0,1320,0C1280,0,1240,0,1200,0C1160,0,1120,0,1080,0C1040,0,1000,0,960,0C920,0,880,0,840,0C800,0,760,0,720,0C680,0,640,0,600,0C560,0,520,0,480,0C440,0,400,0,360,0C320,0,280,0,240,0C200,0,160,0,120,0C80,0,40,0,20,0L0,0Z"
-          ></path>
-        </svg>
-      </div>
+    
+     
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,157 @@ export default function Home() {
   return (
     <div>
       <Header />
-      {/* <Footer /> */}
+
+  <div>
+ 
+      <div className="WHAT WE CAN DO  justify-center text-center flex flex-wrap bg-white  -mt-4  mb-24">
+          <div className="w-full md:w-6/12 px-12 md:px-4">
+            <h2 className="font-semibold text-4xl">Why does your business need us</h2>
+            <p className="text-lg leading-relaxed mt-4 mb-4 text-blueGray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fuga distinctio, ipsum eos 
+              quisquam ea deleniti Necessitatibus tenetur eum, porro minima accusantium praesentium 
+              itaque harum, magnam ea, atque recusandae.
+            </p>
+          </div>
+        </div>
+
+
+      <section className="block relative z-1 bg-blueGray-600">
+        <div className="container mx-auto">
+          <div className="justify-center flex flex-wrap">
+            <div className="w-full lg:w-12/12 px-4  -mt-24">
+              <div className="flex flex-wrap">
+                <div className="w-full lg:w-4/12 px-4">
+                  <h5 className="text-xl font-semibold pb-4 text-center">
+                    Wed developement 
+                  </h5>
+                    <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                      <img
+                        alt="..."
+                        className="align-middle border-none max-w-full h-auto rounded-lg"
+                        src= '/service2.jpg'
+                      />
+                    </div>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus nihi
+                    commodi obcaecati nam officia.Recusandae illum,
+                     repudiandae odit qui numquam, pariatur officia dolore quam dolorem delectus, iure nemo est aperiam.</p>
+                </div>
+
+                <div className="w-full lg:w-4/12 px-4">
+                  <h5 className="text-xl font-semibold pb-4 text-center">
+                    Data service 
+                  </h5>
+                
+                    <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                      <img
+                        alt="..."
+                        className="align-middle border-none max-w-full h-auto rounded-lg"
+                        src='/service6.jpg'
+                      />
+                    </div>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Corporis minima voluptatibus error sunt hic repellat modi iure.
+                  Nobis, neque, eaque aliquam aspernatur, repellat modi molestiae 
+                  pariatur beatae veniam a minima?</p>
+                </div>
+
+                <div className="w-full lg:w-4/12 px-4">
+                  <h5 className="text-xl font-semibold pb-4 text-center">
+                    Consultancy services
+                  </h5>
+                  
+                    <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                      <img
+                        alt="..."
+                        className="align-middle border-none max-w-full h-auto rounded-lg"
+                        src='service7.jpg'
+                      />
+                    </div>
+                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro,
+                    repudiandae ab obcaecati exercitationem alias consequatur similique
+                    ullam facere iure officia consequuntur, libero dicta officiis eligendi 
+                    modi laboriosam! Eveniet, distinctio.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+  </div>
+<section className='WHO ARE WE container mx-auto overflow-hidden pb-20'>
+<div className="flex flex-wrap items-center pt-32">
+<div className="w-full md:w-4/12 px-12 md:px-4 ml-auto mr-auto mt-48">
+             
+              <h3 className="text-3xl mb-2 font-semibold leading-normal">
+                Who are we
+              </h3>
+              <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-blueGray-600">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem in harum 
+                maiores quod recusandae tenetur obcaecati numquam,
+                 natus quia repudiandae labore praesentium quam, odit cumque quasi repellat ipsam sint distinctio.
+              </p>
+              
+             
+            </div>
+            <div className="w-full md:w-6/12 px-4 mr-auto ml-auto mt-32">
+              <div className="justify-center flex flex-wrap relative">
+                <div className="my-4 w-full lg:w-6/12 px-4">
+                 
+                    <div className="bg-red-600 shadow-lg rounded-lg text-center p-8">
+                     
+                      <p className="text-lg text-white mt-4 font-semibold">
+                        our project
+                      </p>
+                    </div>
+                  
+                  
+                   
+                
+                 
+                    <div className="bg-blueGray-700 shadow-lg rounded-lg text-center p-8 mt-8 bg-gray-700">
+                      <img
+                        
+                      />
+                      <p className="text-lg text-white mt-4 font-semibold">
+                        client working on ir ]
+                      </p>
+                    </div>
+                 
+                </div>
+                <div className="my-4 w-full lg:w-6/12 px-4 lg:mt-16">
+                  <a
+                    href="https://www.creative-tim.com/learning-lab/tailwind/js/alerts/notus?ref=vtw-index"
+                    target="_blank"
+                  >
+                    <div className="bg-yellow-500 shadow-lg rounded-lg text-center p-8">
+                      <img
+                        
+                      />
+                      <p className="text-lg text-white mt-4 font-semibold">
+                       cients stisfaction
+                      </p>
+                    </div>
+                  </a>
+                 
+                    <div className="bg-red-700 shadow-lg rounded-lg text-center p-8 mt-8">
+                      <img
+                        
+                      />
+                      <p className="text-lg text-white mt-4 font-semibold">
+                        data let see hahah
+                      </p>
+                    </div>
+                 
+                </div>
+              </div>
+            </div>
+
+            
+          </div>
+</section>
+
+
+     {/* <Footer />  */}
     </div>
   );
 }
